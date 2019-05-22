13249584ff434ebaca5e5a648551fa4630dea4ab
arpit2304 <soniarpi@sheridancollege.ca>
2019-05-22 18:57:28 -0400

    Student demo

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  *
- * @author Sivagami
+ * @author Arpitkumar Soni
  */
 public class CardTrick
 {
@@ -18,6 +18,12 @@ public class CardTrick
    public static void main (String[] args)
    {
       Card[] magicHand = new Card[7];
+
+      Scanner s = new Scanner(System.in);
+      System.out.println("add card suit");
+      String userSuit = s.nextLine();
+      System.out.println("Add your card number ");
+      int uservalue = s.nextInt();
 
       for (int i = 0; i < magicHand.length; i++) {
          Card c = new Card();
@@ -29,10 +35,22 @@ public class CardTrick
          //c.setValue(insert call to random number generator here)
          //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
       }
-      Scanner s = new Scanner(System.in);
-      int uservalue = s.nextInt();
-      String userSuit = s.nextLine();
 
+
+      for (int i = 0; i < magicHand.length; i++) {
+         int num = magicHand[i].getValue();
+         String su = magicHand[i].getSuit();
+         if (userSuit == su && uservalue == num) {
+            System.out.println("Your card matched!!!");
+            break;
+         }
+         else {
+            if (i == magicHand.length - 1) {
+               System.out.println("No cards matched");
+            }
+         }
+
+      }
       //insert code to ask the user for Card value and suit, create their card
       // and search magicHand here
       //Then report the result here
