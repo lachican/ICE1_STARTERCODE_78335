5278cae9f055feff26bcee738ca479b8167b0c0b
arpit2304 <50675925+arpit2304@users.noreply.github.com>
2019-05-23 11:36:14 -0400

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  *
- * @author Arpitkumar Soni
+ * @author Arpitkumar Soni(991550760)
  */
 public class CardTrick
 {
