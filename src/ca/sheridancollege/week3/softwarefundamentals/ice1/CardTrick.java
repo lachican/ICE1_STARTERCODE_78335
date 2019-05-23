9881b73f271bca8ed88f2eba647243a4fe67acd8
tbozdemir <bozdemir@sheridancollege.ca>
2019-05-23 11:30:59 -0400

    Student Demo

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -3,7 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package student.template;
+package ca.sheridancollege.week3.softwarefundamentals.ice1;
+
 import java.util.*;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
