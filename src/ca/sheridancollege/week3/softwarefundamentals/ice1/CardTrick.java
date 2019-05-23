275014e641e46bc4603b5abc8d2ed3a540c7892a
tbozdemir <bozdemir@sheridancollege.ca>
2019-05-23 11:27:22 -0400

    Student Demo

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -3,8 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
+package student.template;
+import java.util.*;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -13,14 +13,23 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  */
 public class CardTrick {
     
-    public static void main(String[] args)
+     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+         Card luckyCard=new Card();
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
+            c.setValue(c.randomVal());
+            
+            c.setSuit(Card.SUITS[c.randomSuit()]);
+            magicHand[i]=c;
+            System.out.println(magicHand[i].getSuit()+magicHand[i].getValue());
+
+
+
+//c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
         
